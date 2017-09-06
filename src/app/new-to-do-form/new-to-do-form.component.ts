6a66033cd2964206aa8e3e11971263c30b6e88b1
LAPTOP-BSADRRE6\DerekLiao <ai015606220@gmail.com>
2017-09-06 08:46:15 +0800

    modify form

--- src/app/new-to-do-form/new-to-do-form.component.ts
+++ src/app/new-to-do-form/new-to-do-form.component.ts
@@ -28,12 +28,8 @@ export class NewToDoFormComponent implements OnInit {
 
   userList: any;
 
-  newItem: any = new newToDoItem(
-    '',
-    'Normal',
-    12,
-    ''
-  );
+  // set default vaule of form in first time
+  newItem: any = this.setDefault();
 
   ngOnInit() {
     this.dataService.getUserList()
@@ -50,12 +46,25 @@ export class NewToDoFormComponent implements OnInit {
     );
   }
 
-  addItem() {
-    this.newToDoItem.emit(this.newItem);
+  addItem(form) {
+    this.newToDoItem.emit(form.value);
+    // set default vaule of form
+    const defaultValue = this.setDefault();
+    // reset form
+    form.resetForm(defaultValue);
   }
 
   toJsonString(obj) {
     return JSON.stringify(obj);
+  }
+
+  setDefault() {
+    return new newToDoItem(
+      '',
+      'Normal',
+      12,
+      ''
+    );
   }
 
 }
